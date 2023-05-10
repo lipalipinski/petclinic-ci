ce1ac2518477c67a91d7da9b29c50ee68ca6aef1
lipalipinski <lipabass@gmail.com>
2023-05-10 23:08:05 +0200

    mend

--- scripts/check-site.sh
+++ scripts/check-site.sh
@@ -6,7 +6,7 @@ interval=2
 tout=60
 port=$1
 
-for (( i=0; i<$tout; i+=$step ))
+for (( i=0; i<$tout; i+=$interval ))
 do 
   resp=$(curl -s -I localhost:$port | grep "HTTP/1.1")
   if [[ $resp == *"200"* ]]
@@ -14,8 +14,8 @@ do
     echo "$resp"
     exit 0
   fi
-  echo "waiting... $(( $i + $step ))s"
-  sleep $step
+  echo "waiting... $(( $i + $interval ))s"
+  sleep $interval
 done
 
 echo "$resp"
