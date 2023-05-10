35442d455b23defeb40e02159f3067704f596039
lipalipinski <lipabass@gmail.com>
2023-05-10 23:01:55 +0200

    mend

--- scripts/check-site.sh
+++ scripts/check-site.sh
@@ -8,7 +8,7 @@ port="$1"
 
 for (( i=0; i<$tout; i+=$step ))
 do 
-  resp=$(curl -s -I localhost:$port | grep "HTTP/1.1")
+  resp=$(curl -s -I localhost:"$port" | grep "HTTP/1.1")
   if [[ $resp == *"200"* ]]
   then
     echo "$resp"
