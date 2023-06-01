8a2d147c1ec76a4e9309bbc4f7f1c91da0b9009a
Jakub Lipiński <98739871+lipalipinski@users.noreply.github.com>
2023-06-01 23:12:40 +0200

    checksite.sh hostip

--- scripts/check-site.sh
+++ scripts/check-site.sh
@@ -2,13 +2,14 @@
 
 # check if server responds on a given port
 
+hostip=$(/sbin/ip route|awk '/default/ { print $3 }')
 interval=2
 tout=60
 port=$1
 
 for (( i=0; i<$tout; i+=$interval ))
 do 
-  resp=$(curl -s -I localhost:$port | grep "HTTP/1.1")
+  resp=$(curl -s -I $hostip:$port | grep "HTTP/1.1")
   if [[ $resp == *"200"* ]]
   then
     echo "$resp"
@@ -19,5 +20,5 @@ do
 done
 
 echo "$resp"
-echo "Site unavailable at http://localhost:$port"
+echo "Site unavailable at http://$hostip:$port"
 exit 62
